d46d1cf518764065a94f3a7625307189a8a6fc64
santabomb5 <santabomb5@JUNKRAT>
2022-06-29 22:38:30 -0500

    Add project files.

---
+++ WordSearcher/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace WordSearcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listofwords = new List<string> { "test", "test", "another word" };
+            Console.Write("What word are you looking for?");
+            string testWord = Console.ReadLine();
+
+            int i = 0;
+            foreach (string word in listofwords)
+            {
+               if (word.ToUpper() == testWord.ToUpper())
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine(testWord + " showed up " + i + " times!");
+        }
+    }
+}
